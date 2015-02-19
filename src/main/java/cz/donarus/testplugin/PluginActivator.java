b7b7e58004d818513d76856ac49c2af509a230ab
donarus <tadeas@palusga.cz>
2015-02-19 02:29:49 +0100

    added some optional dependency and implemented fake IPluginActivator

--- src/main/java/cz/donarus/testplugin/PluginActivator.java
+++ src/main/java/cz/donarus/testplugin/PluginActivator.java
@@ -1,4 +1,21 @@
 package cz.donarus.testplugin;
 
-public class PluginActivator {
+import cz.cuni.mff.d3s.been.pluger.IPluginActivator;
+import cz.cuni.mff.d3s.been.pluger.IServiceRegistrator;
+
+public class PluginActivator implements IPluginActivator {
+    @Override
+    public void activate(IServiceRegistrator registry) {
+        System.out.println("activating");
+    }
+
+    @Override
+    public void initialize() {
+        System.out.println("initializing");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("starting");
+    }
 }
